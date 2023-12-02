17191f1ce2c621f4a9e4dfedcdffb477bd0c0252
Steakhouse Spency <steakhouse@steakhousesair2.mynetworksettings.com>
2023-12-01 19:46:24 -0800

    [github] script to generate and register new ssh key

---
+++ git/add_sshkey.sh
@@ -0,0 +1,31 @@
+# !/bin/bash
+
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account
+email="<insert email"
+
+function steak(){
+	echo "\n🥩🥩🥩\n$1\n🥩🥩🥩\n"
+}
+
+ssh_key_filename="github_ssh_key"
+
+steak "creating ssh key - press enter when prompted"
+ssh-keygen -t ed25519 -C $email -f ~/.ssh/$ssh_key_filename
+
+
+steak "starting ssh agent"
+eval "$(ssh-agent -s)"
+
+steak "updating ssh config file"
+touch ~/.ssh/config
+echo "
+Host github.com
+  AddKeysToAgent yes
+  UseKeychain yes
+  IdentityFile ~/.ssh/$ssh_key_filename
+" >> ~/.ssh/config
+
+cat ~/.ssh/config
+
+steak "adding ssh key to keychain"
+ssh-add --apple-use-keychain ~/.ssh/$ssh_key_filename
